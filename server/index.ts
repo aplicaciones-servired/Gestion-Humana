c01412de65e879c88ba8c927c906c0787f9998bf
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2025-12-15 15:37:40 -0500

    Actualizar configuración del servidor y scripts de inicio en package.json

--- server/index.ts
+++ server/index.ts
@@ -11,10 +11,10 @@ app.use(express.json())
 
 app.use(RoutesGET)
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
-})
-
 app.get('/', (req, res) => {
   res.send('Hello, World!')
+})
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server is running on http://0.0.0.0:3000')
 })
